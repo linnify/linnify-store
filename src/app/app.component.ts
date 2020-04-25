78cb1ac802b275a684c9df8cc5d4b211f89b1084
razvanbretoiu <razvan.bretoiu@linnify.com>
2020-04-25 12:05:05 +0300

    style: Add products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="container">
+      <div class="header">
+        <h1>{{ title }}</h1>
+        <h3>Buy our Digital EAU DE PERFUME</h3>
+      </div>
+
+      <h3> Our Products </h3>
+    </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'linnify-store';
+export class AppComponent implements OnInit{
+  title = 'Welcome to Linnify Store';
+
+  products = [
+    {
+      id: 1,
+      name: 'Linnify Fragrance',
+      price: 49.99,
+      image: 'https://storage.googleapis.com/company-app-1276d.appspot.com/linnify-store/1.png',
+      available: true
+    },
+    {
+      id: 2,
+      name: 'Linnify Digital Perfume',
+      price: 90,
+      image: 'https://storage.googleapis.com/company-app-1276d.appspot.com/linnify-store/2.png',
+      available: true
+    },
+    {
+      id: 3,
+      name: 'Linnify Experience',
+      price: 150.4,
+      image: 'https://storage.googleapis.com/company-app-1276d.appspot.com/linnify-store/3.png',
+      available: false
+    },
+    {
+      id: 4,
+      name: 'Linnify Perfume for Men',
+      price: 74.4,
+      image: 'https://storage.googleapis.com/company-app-1276d.appspot.com/linnify-store/4.png',
+      available: true
+    }
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
